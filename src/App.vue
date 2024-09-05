6f99e05d9165914203999700e6e10d2015e2008a
JBdevCristian <jbcristian78@gmail.com>
2024-09-05 19:18:15 -0300

    v-if

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template>
   <div id="app">
-    <input type="text" v-model="cliente.nome">
-    <input type="text" v-model="cliente.email">
+    <input type="text" v-model="clienteRogerio.nome">
+    <input type="text" v-model="clienteRogerio.email">
     <h1>Guia Clientes</h1>
-    <Cliente nome="rodolfo" email="teste@gmail.com" idade="50"/>
-    <Cliente :nome="cliente.nome" :email="cliente.email" :idade="cliente.idade"/>
-    <Cliente/>
-    <Cliente/>
+    <Cliente :cliente="clienteRogerio" :showIdade="false" descicao="testando prop"/>
 
   </div>
 </template>
@@ -19,7 +16,7 @@ export default {
   name: 'app',
   data() {
     return {
-      cliente: {
+      clienteRogerio: {
         nome: "Rogerio",
         idade: 25,
         email: "paypalteste@gmail.com"
--- src/components/ClienteTeste.vue
+++ src/components/ClienteTeste.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="cadastro">
         <h1>Informações do Cliente</h1>
-        <h4>Nome do cliente: {{ nome }}</h4>
-        <p>Descrição: {{ descricao }}</p>
-        <p>E-mail: {{ email }}</p>
-        <p>Idade do cliente: {{ idade }}</p>
+        <h4>Nome do cliente: {{ cliente.nome }}</h4>
+        <p>E-mail: {{ cliente.email }}</p>
+        <p>Descição: {{ descicao }}</p>
+        <p v-if="showIdade">Idade do cliente: {{ cliente.idade }}</p>
+        <p v-else>O cliente decidiu esconder a idade!!</p>
     </div>
 </template>
 
@@ -12,14 +13,12 @@
 export default {
     data() {
         return {
-        descricao: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
-        }
+       }
     },
     props: { //sempre passar o objeto props
-        nome: String, //definir tipo
-        email: String, //definir tipo
-        idade: Number, //definir tipo
-        cliente: Object //definir tipo
+        cliente: Object, //definir tipo
+        showIdade: Boolean,
+        descicao: String
 
     }
 }
