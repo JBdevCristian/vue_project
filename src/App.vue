5306bd651aa03902e8b0df6d8f12bf20dae4e56f
JBdevCristian <jbcristian78@gmail.com>
2024-09-05 17:59:20 -0300

    Reatividade

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Guia Clientes</h1>
+    <Cliente/>
+    <Produto/>
+    <Cliente/>
+    <Produto/>
+    <Cliente/>
+    <Produto/>
+    <Cliente/>
+    <Produto/>
+    <Cliente/>
+    <Produto/>
+    <Cliente/>
+    <Produto/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cliente from './components/ClienteTeste.vue'
+//import Produto from './components/ProdutoTeste.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Cliente,
+    //Produto
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
