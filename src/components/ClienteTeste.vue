34b9b23d9b5e6c65e76457eddfa38980e67b4405
JBdevCristian <jbcristian78@gmail.com>
2024-09-05 18:14:16 -0300

    two way data biding

--- src/components/ClienteTeste.vue
+++ src/components/ClienteTeste.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <h1>Informações do Cliente</h1>
-        <input type="text" :value="nome">
+        <input type="text" v-model="nome">
         <hr>
-        <input type="text" :value="nacionalidade">
+        <input type="text" v-model="nacionalidade">
         <hr>    
         <p>Nome do cliente: {{ nome }}</p>
         <p>Idade do cliente: {{ idade }}</p>
